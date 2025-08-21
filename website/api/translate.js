7615be74980eff66009de771edb60e776d4d18bb
Astitva23 <astitvaa.k07@gmail.com>
2025-08-21 20:38:47 +0530

    api

--- website/api/translate.js
+++ website/api/translate.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-const dictPath = path.join(process.cwd(), "..", "dictionary.json"); // go one level up
+const dictPath = path.join(process.cwd(), "dictionary.json");
 const BRAINROT_DICT = JSON.parse(fs.readFileSync(dictPath, "utf-8"));
 
 export default function handler(req, res) {
